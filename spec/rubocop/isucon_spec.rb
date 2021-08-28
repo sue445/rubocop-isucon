0ed7e9722ab750bb686aabf7b9d639818c03fc0c
sue445 <sue445@sue445.net>
2021-08-28 20:18:40 +0900

    Fix test

--- spec/rubocop/isucon_spec.rb
+++ spec/rubocop/isucon_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Rubocop::Isucon do
+RSpec.describe RuboCop::Isucon do
   it "has a version number" do
-    expect(Rubocop::Isucon::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(RuboCop::Isucon::VERSION).not_to be nil
   end
 end
