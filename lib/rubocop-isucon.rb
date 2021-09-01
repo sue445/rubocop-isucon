c27f632f3b7b04a9bb355914e0f0af7a3c772d40
sue445 <sue445@sue445.net>
2021-09-02 00:11:52 +0900

    Impl SqlParser.parse_tables

--- lib/rubocop-isucon.rb
+++ lib/rubocop-isucon.rb
@@ -7,6 +7,7 @@ require_relative "rubocop/isucon"
 require_relative "rubocop/isucon/version"
 require_relative "rubocop/isucon/inject"
 require_relative "rubocop/isucon/database_connection"
+require_relative "rubocop/isucon/sql_parser"
 
 RuboCop::Isucon::Inject.defaults!
 
