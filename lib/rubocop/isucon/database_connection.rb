1aee951a3baee8bd4021cbc842a8f971d3098d45
sue445 <sue445@sue445.net>
2021-09-02 13:14:25 +0900

    Refactor: column_names caching

--- lib/rubocop/isucon/database_connection.rb
+++ lib/rubocop/isucon/database_connection.rb
@@ -7,13 +7,16 @@ module RuboCop
       # @param [Hash]
       def initialize(database_config)
         ActiveRecord::Base.establish_connection(database_config)
+        @column_names_by_table = {}
       end
 
       # @param table_name [String]
       # @return [Array<String>]
       def column_names(table_name)
+        return @column_names_by_table[table_name] if @column_names_by_table[table_name]
+
         columns = ActiveRecord::Base.connection.columns(table_name)
-        columns.map(&:name)
+        @column_names_by_table[table_name] = columns.map(&:name)
       end
     end
   end
