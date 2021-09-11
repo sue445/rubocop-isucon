f1027b6aae76a8c990b3a139ede121dbf9a4ec5d
sue445 <sue445@sue445.net>
2021-09-11 16:48:10 +0900

    Impl #table_names

--- lib/rubocop/isucon/gda_helper.rb
+++ lib/rubocop/isucon/gda_helper.rb
@@ -7,9 +7,31 @@ module RuboCop
       PRACEHOLDER = "'__PRACEHOLDER__'"
 
       # @param sql [String]
+      def initialize(sql)
+        @sql = sql
+      end
+
+      # @return [Array<String>]
+      def table_names
+        ast.from.targets.map(&:table_name)
+      end
+
+      # @param sql [String]
       # @return [String]
       def self.normalize_sql(sql)
         sql.gsub("`", "").gsub("?", PRACEHOLDER)
+      end
+
+      private
+
+      # @return [GDA::SQL::Statement]
+      def statement
+        @statement ||= GDA::SQL::Parser.new.parse(self.class.normalize_sql(@sql))
+      end
+
+      # @return [GDA::Nodes::Select]
+      def ast
+        @ast ||= statement.ast
       end
     end
   end
--- spec/rubocop/isucon/gda_helper_spec.rb
+++ spec/rubocop/isucon/gda_helper_spec.rb
@@ -1,6 +1,45 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Isucon::GdaHelper do
+  let(:helper) { RuboCop::Isucon::GdaHelper.new(sql) }
+
+  describe "#table_names" do
+    subject { helper.table_names }
+
+    context "single table" do
+      let(:sql) do
+        # https://github.com/isucon/isucon10-qualify/blob/7e6b6cfb672cde2c57d7b594d0352dc48ce317df/webapp/ruby/app.rb#L118
+        <<~SQL
+          SELECT * FROM chair WHERE stock > 0 ORDER BY price ASC, id ASC LIMIT 10
+        SQL
+      end
+
+      it { should contain_exactly("chair") }
+    end
+
+    context "multiple tables" do
+      let(:sql) do
+        <<~SQL
+          SELECT
+            r.id AS reservation_id,
+            r.schedule_id AS reservation_schedule_id,
+            r.user_id AS reservation_user_id,
+            r.created_at AS reservation_created_at,
+            u.id AS user_id,
+            u.email AS user_email,
+            u.nickname AS user_nickname,
+            u.staff AS user_staff,
+            u.created_at AS user_created_at
+          FROM `reservations` AS r
+          INNER JOIN users u ON u.id = r.user_id
+          WHERE r.schedule_id = ?
+        SQL
+      end
+
+      it { should contain_exactly("reservations", "users") }
+    end
+  end
+
   describe ".normalize_sql" do
     subject { RuboCop::Isucon::GdaHelper.normalize_sql(sql) }
 
