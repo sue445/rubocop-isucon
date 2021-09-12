4739284f2ee074221dd048dd00cf655e297ae02a
sue445 <sue445@sue445.net>
2021-09-12 13:22:41 +0900

    Move to mysql2

--- lib/rubocop/cop/isucon_cops.rb
+++ lib/rubocop/cop/isucon_cops.rb
@@ -4,5 +4,5 @@ require_relative "isucon/mixin/database_methods"
 
 require_relative "isucon/mysql2/select_asterisk"
 require_relative "isucon/mysql2/n_plus_one_query"
+require_relative "isucon/mysql2/where_without_index"
 require_relative "isucon/sinatra/disable_logging"
-require_relative 'isucon/where_without_index'
--- spec/rubocop/cop/isucon/mysql2/where_without_index_spec.rb
+++ spec/rubocop/cop/isucon/mysql2/where_without_index_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe RuboCop::Cop::Isucon::WhereWithoutIndex, :config do
+RSpec.describe RuboCop::Cop::Isucon::Mysql2::WhereWithoutIndex, :config do
   let(:config) { RuboCop::Config.new }
 
   # TODO: Write test code
