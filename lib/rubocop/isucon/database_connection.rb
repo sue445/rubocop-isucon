010cc7ccab8297e6d65ac629e8110935dd87194f
sue445 <sue445@sue445.net>
2021-09-12 14:40:09 +0900

    Impl indexes

--- lib/rubocop/isucon/database_connection.rb
+++ lib/rubocop/isucon/database_connection.rb
@@ -8,6 +8,7 @@ module RuboCop
       def initialize(database_config)
         ActiveRecord::Base.establish_connection(database_config)
         @column_names_by_table = {}
+        @indexes_by_table = {}
       end
 
       # @param table_name [String]
@@ -17,6 +18,15 @@ module RuboCop
 
         columns = ActiveRecord::Base.connection.columns(table_name)
         @column_names_by_table[table_name] = columns.map(&:name)
+      end
+
+      # @param table_name [String]
+      # @return [Array<ActiveRecord::ConnectionAdapters::IndexDefinition>]
+      # @see [ActiveRecord::ConnectionAdapters::TableDefinition#indexes]
+      def indexes(table_name)
+        return @indexes_by_table[table_name] if @indexes_by_table[table_name]
+
+        @indexes_by_table[table_name] = ActiveRecord::Base.connection.indexes(table_name)
       end
     end
   end
