b1880100faa8460940fa568f1922d3fb4029ce00
sue445 <sue445@sue445.net>
2021-09-12 16:13:53 +0900

    Refactor: Extract to exists_index_in_where_clause_columns?

--- lib/rubocop/cop/isucon/mysql2/where_without_index.rb
+++ lib/rubocop/cop/isucon/mysql2/where_without_index.rb
@@ -24,7 +24,7 @@ module RuboCop
             (send (send nil? _) {:xquery | :query} (str $_) ...)
           PATTERN
 
-          def on_send(node) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+          def on_send(node) # rubocop:disable Metrics/MethodLength
             return unless enabled_database?
 
             find_xquery(node) do |sql|
@@ -36,16 +36,8 @@ module RuboCop
               next unless table_names.count == 1
 
               table_name = table_names[0]
-              indexes = connection.indexes(table_name)
-              index_first_columns = indexes.map { |index| index.columns[0] }
 
-              next if gda.where_clause.any? { |condition| index_first_columns.include?(condition.column_operand) }
-
-              primary_keys = connection.primary_keys(table_name)
-              unless primary_keys.empty?
-                where_columns = gda.where_clause.map(&:column_operand)
-                next if primary_keys.all? { |column| where_columns.include?(column) }
-              end
+              next if exists_index_in_where_clause_columns?(gda, table_name)
 
               loc = sql_where_location(node, sql)
               next unless loc
@@ -69,6 +61,24 @@ module RuboCop
             end_pos = begin_pos + 5
 
             Parser::Source::Range.new(node.loc.expression.source_buffer, begin_pos, end_pos)
+          end
+
+          # @param gda [RuboCop::Isucon::GdaHelper]
+          # @param table_name [String]
+          # @return [Boolean]
+          def exists_index_in_where_clause_columns?(gda, table_name) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity
+            indexes = connection.indexes(table_name)
+            index_first_columns = indexes.map { |index| index.columns[0] }
+
+            return true if gda.where_clause.any? { |condition| index_first_columns.include?(condition.column_operand) }
+
+            primary_keys = connection.primary_keys(table_name)
+            unless primary_keys.empty?
+              where_columns = gda.where_clause.map(&:column_operand)
+              return true if primary_keys.all? { |primary_key| where_columns.include?(primary_key) }
+            end
+
+            false
           end
         end
       end
