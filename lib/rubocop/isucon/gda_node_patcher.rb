531bfea39fa8f5b2cd6ff475db2d1f9dadc18d39
sue445 <sue445@sue445.net>
2021-09-16 23:32:15 +0900

    Impl GdaNodePatcher

---
+++ lib/rubocop/isucon/gda_node_patcher.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Isucon
+    # Monkey patching to #{GDA::Nodes::Node}
+    class GdaNodePatcher < GDA::Visitors::Visitor
+      # @param node [GDA::Nodes::Node]
+      # @param sql [String]
+      def accept(node, sql)
+        @sql = sql
+        @current_pos = 0
+        super(node)
+      end
+
+      private
+
+      def visit_GDA_Nodes_Operation(node) # rubocop:disable Naming/MethodName,Metrics/MethodLength
+        return super unless node.operator
+
+        pattern =
+          case node.operands.count
+          when 1
+            /#{node.operands[0].value}\s*#{node.operator}/
+          when 2
+            /#{node.operands[0].value}\s*#{node.operator}\s*#{node.operands[1].value}/
+          else
+            return super
+          end
+
+        node.location = search_location(pattern)
+
+        super
+      end
+
+      # @param [Regexp] pattern
+      # @return [RuboCop::Isucon::GdaNodeLocation]
+      def search_location(pattern)
+        begin_pos = @sql.index(pattern, @current_pos)
+        length = Regexp.last_match[0].length
+        end_pos = begin_pos + length
+        @current_pos = end_pos
+
+        GdaNodeLocation.new(begin_pos: begin_pos, end_pos: end_pos, body: Regexp.last_match[0])
+      end
+    end
+  end
+end
