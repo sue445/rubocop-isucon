30d7082b7744234a52ced8958ac2fdf2c9b0cfb8
sue445 <sue445@sue445.net>
2021-09-24 00:53:05 +0900

    Impl

--- lib/rubocop/cop/isucon/mysql2/join_without_index.rb
+++ lib/rubocop/cop/isucon/mysql2/join_without_index.rb
@@ -4,57 +4,92 @@ module RuboCop
   module Cop
     module Isucon
       module Mysql2
-        # TODO: Write cop description and example of bad / good code. For every
-        # `SupportedStyle` and unique configuration, there needs to be examples.
-        # Examples must have valid Ruby syntax. Do not use upticks.
+        # Check for `JOIN` without index
         #
-        # @example EnforcedStyle: bar (default)
-        #   # Description of the `bar` style.
+        # @note If `Database` isn't configured, this cop's feature (offense detection and auto-correct) will not be available.
         #
-        #   # bad
-        #   bad_bar_method
+        # @example
+        #   # bad (user_id is not indexed)
+        #   db.xquery('SELECT id, title FROM articles JOIN users ON users.id = articles.user_id')
         #
-        #   # bad
-        #   bad_bar_method(args)
-        #
-        #   # good
-        #   good_bar_method
-        #
-        #   # good
-        #   good_bar_method(args)
-        #
-        # @example EnforcedStyle: foo
-        #   # Description of the `foo` style.
-        #
-        #   # bad
-        #   bad_foo_method
-        #
-        #   # bad
-        #   bad_foo_method(args)
-        #
-        #   # good
-        #   good_foo_method
-        #
-        #   # good
-        #   good_foo_method(args)
+        #   # good (user_id is indexed)
+        #   db.xquery('SELECT id, title FROM articles JOIN users ON users.id = articles.user_id')
         #
         class JoinWithoutIndex < Base
-          # TODO: Implement the cop in here.
-          #
-          # In many cases, you can use a node matcher for matching node pattern.
-          # See https://github.com/rubocop/rubocop-ast/blob/master/lib/rubocop/ast/node_pattern.rb
-          #
-          # For example
-          MSG = 'Use `#good_method` instead of `#bad_method`.'
+          include Mixin::DatabaseMethods
+          include Mixin::Mysql2Methods
 
-          def_node_matcher :bad_method?, <<~PATTERN
-            (send nil? :bad_method ...)
-          PATTERN
+          MSG = "This join clause doesn't seem to have an index. " \
+                "(e.g. 'ALTER TABLE `%<table_name>s` ADD INDEX `index_%<column_name>s` (%<column_name>s)')"
 
+          # @param node [RuboCop::AST::Node]
           def on_send(node)
-            return unless bad_method?(node)
+            return unless enabled_database?
 
-            add_offense(node)
+            find_xquery(node) do |type, params|
+              sql = xquery_param(type: type, params: params)
+
+              check_and_register_offence(type: type, sql: sql, node: node)
+            end
+          end
+
+          private
+
+          # @param type [Symbol] one of `:str`, `:dstr`
+          # @param sql [String]
+          # @param node [RuboCop::AST::Node]
+          def check_and_register_offence(type:, sql:, node:)
+            root_gda = RuboCop::Isucon::GDA::Client.new(sql)
+
+            root_gda.visit_all do |gda|
+              gda.join_conditions.each do |join_condition|
+                join_operand = join_operand_without_index(join_condition)
+                next unless join_operand
+
+                register_offense(type: type, node: node, join_operand: join_operand)
+              end
+            end
+          end
+
+          # @param join_condition [RuboCop::Isucon::GDA::JoinCondition]
+          # @return [RuboCop::Isucon::GDA::JoinOperand,nil]
+          def join_operand_without_index(join_condition)
+            join_condition.operands.each do |join_operand|
+              unless indexed_column?(table_name: join_operand.table_name, column_name: join_operand.column_name)
+                return join_operand
+              end
+            end
+
+            nil
+          end
+
+          # @param table_name [String]
+          # @param column_name [String]
+          # @return [Boolean]
+          def indexed_column?(table_name:, column_name:)
+            primary_keys = connection.primary_keys(table_name)
+
+            return true if primary_keys&.first == column_name
+
+            indexes = connection.indexes(table_name)
+            index_first_columns = indexes.map { |index| index.columns[0] }
+            index_first_columns.include?(column_name)
+          end
+
+          # @param type [Symbol] one of `:str`, `:dstr`
+          # @param node [RuboCop::AST::Node]
+          # @param join_operand [RuboCop::Isucon::GDA::JoinOperand]
+          def register_offense(type:, node:, join_operand:)
+            loc = offense_location(type: type, node: node, gda_location: join_operand.node.location)
+            return unless loc
+
+            message = offense_message(join_operand)
+            add_offense(loc, message: message)
+          end
+
+          # @param join_operand [RuboCop::Isucon::GDA::JoinOperand]
+          def offense_message(join_operand)
+            format(MSG, table_name: join_operand.table_name, column_name: join_operand.column_name)
           end
         end
       end
--- spec/rubocop/cop/isucon/mysql2/join_without_index_spec.rb
+++ spec/rubocop/cop/isucon/mysql2/join_without_index_spec.rb
@@ -1,21 +1,54 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Isucon::Mysql2::JoinWithoutIndex, :config do
-  let(:config) { RuboCop::Config.new }
+  let(:config) { RuboCop::Config.new("Isucon/Mysql2/JoinWithoutIndex" => cop_config) }
+  let(:cop_config) { {} }
 
-  # TODO: Write test code
-  #
-  # For example
-  it 'registers an offense when using `#bad_method`' do
-    expect_offense(<<~RUBY)
-      bad_method
-      ^^^^^^^^^^ Use `#good_method` instead of `#bad_method`.
-    RUBY
+  context "without index" do
+    include_context :database_cop do
+      let(:schema) do
+        %w[
+          schemas/create_courses.rb
+          schemas/create_registrations.rb
+        ]
+      end
+    end
+
+    it "registers an offense" do
+      expect_offense(<<~RUBY)
+        courses = db.xquery(
+          "SELECT `courses`.*" \\
+          " FROM `courses`" \\
+          " JOIN `registrations` ON `courses`.`id` = `registrations`.`course_id`" \\
+                                                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^ This join clause doesn't seem to have an index. (e.g. 'ALTER TABLE `registrations` ADD INDEX `index_course_id` (course_id)')
+          " WHERE `courses`.`status` != ? AND `registrations`.`user_id` = ?",
+          STATUS_CLOSED, user_id,
+        )
+      RUBY
+    end
   end
 
-  it 'does not register an offense when using `#good_method`' do
-    expect_no_offenses(<<~RUBY)
-      good_method
-    RUBY
+  context "with index" do
+    include_context :database_cop do
+      let(:schema) do
+        %w[
+          schemas/create_courses.rb
+          schemas/create_registrations.rb
+          schemas/add_index_to_registrations.rb
+        ]
+      end
+    end
+
+    it "does not register an offense" do
+      expect_no_offenses(<<~RUBY)
+        courses = db.xquery(
+          "SELECT `courses`.*" \\
+          " FROM `courses`" \\
+          " JOIN `registrations` ON `courses`.`id` = `registrations`.`course_id`" \\
+          " WHERE `courses`.`status` != ? AND `registrations`.`user_id` = ?",
+          STATUS_CLOSED, user_id,
+        )
+      RUBY
+    end
   end
 end
