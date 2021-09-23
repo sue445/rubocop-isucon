5ea782703d2e3de33d064eec2b6c40daaa820363
GO Sueyoshi <sue445@users.noreply.github.com>
2021-09-23 19:11:12 +0900

    Tweak comment

--- lib/rubocop/isucon/memorize_methods.rb
+++ lib/rubocop/isucon/memorize_methods.rb
@@ -4,19 +4,20 @@ module RuboCop
   module Isucon
     # Memorize helper
     #
-    # @example Usage
-    #   extends RuboCop::Isucon::MemorizeMethods
-    #
-    #   memorize :foo
-    #
-    # @example Generated followings
-    #   def foo_with_cache
-    #     @foo_with_cache ||= foo_without_cache
-    #   end
-    #   alias_method :foo_without_cache, :foo
-    #   alias_method :foo, :foo_with_cache
     module MemorizeMethods
       # @param method_name [String,Symbol]
+      #
+      # @example Usage
+      #   extends RuboCop::Isucon::MemorizeMethods
+      #
+      #   memorize :foo
+      #
+      # @example Generated followings
+      #   def foo_with_cache
+      #     @foo_with_cache ||= foo_without_cache
+      #   end
+      #   alias_method :foo_without_cache, :foo
+      #   alias_method :foo, :foo_with_cache
       def memorize(method_name)
         class_eval <<~RUBY, __FILE__, __LINE__ + 1
           # def foo_with_cache
