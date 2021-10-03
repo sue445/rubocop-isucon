f522a55e24bf2d744f07db718c6f3804312f5e8a
sue445 <sue445@sue445.net>
2021-10-03 19:15:54 +0900

    Support visit_GDA_Nodes_Expr

--- lib/rubocop/isucon/gda/node_patcher.rb
+++ lib/rubocop/isucon/gda/node_patcher.rb
@@ -8,7 +8,8 @@ module RuboCop
         # @param sql [String]
         def initialize(sql)
           @sql = sql
-          @current_pos = 0
+          @current_operation_pos = 0
+          @current_expr_pos = 0
           super()
         end
 
@@ -21,7 +22,7 @@ module RuboCop
           pattern = operand_pattern(node)
           return super unless pattern
 
-          node.location = search_location(pattern)
+          node.location = search_operation_location(pattern)
 
           super
         end
@@ -39,14 +40,37 @@ module RuboCop
 
         # @param pattern [Regexp]
         # @return [RuboCop::Isucon::GDA::NodeLocation]
-        def search_location(pattern)
-          begin_pos = @sql.index(pattern, @current_pos)
+        def search_operation_location(pattern)
+          begin_pos = @sql.index(pattern, @current_operation_pos)
 
           return nil unless Regexp.last_match
 
           length = Regexp.last_match[0].length
           end_pos = begin_pos + length
-          @current_pos = end_pos
+          @current_operation_pos = end_pos
+
+          NodeLocation.new(begin_pos: begin_pos, end_pos: end_pos, body: Regexp.last_match[0])
+        end
+
+        # @param node [GDA::Nodes::Expr]
+        def visit_GDA_Nodes_Expr(node) # rubocop:disable Naming/MethodName -- This method is called from `GDA::Visitors::Visitor#visit` c.f. https://github.com/tenderlove/gda/blob/v1.1.0/lib/gda/visitors/visitor.rb#L13-L17
+          return super unless node.value
+
+          escaped_value = Regexp.escape(node.value)
+          node.location = search_expr_location(/(?<=[\s,])#{escaped_value}(?=[\s,])/)
+          super
+        end
+
+        # @param pattern [Regexp]
+        # @return [RuboCop::Isucon::GDA::NodeLocation]
+        def search_expr_location(pattern)
+          begin_pos = @sql.index(pattern, @current_expr_pos)
+
+          return nil unless Regexp.last_match
+
+          length = Regexp.last_match[0].length
+          end_pos = begin_pos + length
+          @current_expr_pos = end_pos
 
           NodeLocation.new(begin_pos: begin_pos, end_pos: end_pos, body: Regexp.last_match[0])
         end
