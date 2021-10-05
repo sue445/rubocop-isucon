ca49627f2b123ee0dee1d32f17b34ef9aada4dc8
sue445 <sue445@sue445.net>
2021-10-06 00:09:27 +0900

    Removed unused methods

--- lib/rubocop/cop/isucon/mixin/mysql2_methods.rb
+++ lib/rubocop/cop/isucon/mixin/mysql2_methods.rb
@@ -179,48 +179,6 @@ module RuboCop
             indentations.empty? ? 0 : indentations.min_by(&:size).size
           end
 
-          # @param type [Symbol] one of `:str`, `:dstr`
-          # @param node [RuboCop::AST::Node]
-          # @return [Integer,nil]
-          def sql_select_begin_position(type:, node:)
-            case type
-            when :str
-              return sql_select_location_begin_position(node)
-            when :dstr
-              dstr_node = node.child_nodes[1]
-
-              if dstr_node&.dstr_type?
-                if dstr_node.loc.respond_to?(:heredoc_body)
-                  return dstr_node.loc.heredoc_body.begin_pos
-                else
-                  # Parser::Source::Map::Collection
-                  return dstr_node.loc.expression.begin_pos
-                end
-              end
-            end
-            nil
-          end
-
-          # @param type [Symbol] one of `:str`, `:dstr`
-          # @param node [RuboCop::AST::Node]
-          # @param offense_body [String]
-          # @return [Integer]
-          def heredoc_offset(type:, node:, offense_body:)
-            return 0 unless type == :dstr
-
-            heredoc_indent_type = heredoc_indent_type(node)
-            return 0 unless heredoc_indent_type == "~"
-
-            dstr_node = node.child_nodes[1]
-            return 0 if !dstr_node || !dstr_node.dstr_type?
-
-            heredoc_body = dstr_node.loc.heredoc_body.source
-            heredoc_indent_level = indent_level(heredoc_body)
-            line_num = find_line_num(source: heredoc_body, str: offense_body)
-
-            heredoc_indent_level * line_num
-          end
-
           # Returns '~', '-' or nil
           #
           # @param node [RuboCop::AST::Node]
@@ -228,16 +186,6 @@ module RuboCop
           def heredoc_indent_type(node)
             # c.f. https://github.com/rubocop/rubocop/blob/v1.21.0/lib/rubocop/cop/layout/heredoc_indentation.rb#L146-L149
             node.source[/<<([~-])/, 1]
-          end
-
-          # @param source [String]
-          # @param str [String]
-          # @return [Integer]
-          def find_line_num(source:, str:)
-            source.each_line.with_index do |line, i|
-              return i + 1 if line.include?(str)
-            end
-            0
           end
 
           # @param table_names [Array<String>]
