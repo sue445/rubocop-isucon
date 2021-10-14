ffdbc1609dbc998a241c16f069866faed8db36c8
sue445 <sue445@sue445.net>
2021-10-15 00:02:19 +0900

    Remove comment [ci skip]

--- benchmark/memorize.rb
+++ benchmark/memorize.rb
@@ -1,19 +1,6 @@
 require "benchmark/ips"
 
 class DefineMethodMemorizer
-  # @param method_name [String,Symbol]
-  #
-  # @example Usage
-  #   extends RuboCop::Isucon::MemorizeMethods
-  #
-  #   memorize :foo
-  #
-  # @example Generated followings
-  #   def foo_with_cache
-  #     @foo_with_cache ||= foo_without_cache
-  #   end
-  #   alias_method :foo_without_cache, :foo
-  #   alias_method :foo, :foo_with_cache
   def self.memorize(method_name)
     define_method "#{method_name}_with_cache" do
       if (ret = instance_variable_get("@#{method_name}_with_cache"))
@@ -37,19 +24,6 @@ class DefineMethodMemorizer
 end
 
 class ClassEvalMemorizer
-  # @param method_name [String,Symbol]
-  #
-  # @example Usage
-  #   extends RuboCop::Isucon::MemorizeMethods
-  #
-  #   memorize :foo
-  #
-  # @example Generated followings
-  #   def foo_with_cache
-  #     @foo_with_cache ||= foo_without_cache
-  #   end
-  #   alias_method :foo_without_cache, :foo
-  #   alias_method :foo, :foo_with_cache
   def self.memorize(method_name)
     class_eval <<~RUBY, __FILE__, __LINE__ + 1
       # def foo_with_cache
