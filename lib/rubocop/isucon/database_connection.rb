88292dc663abb54ba8f9e84bba73c0993ce81427
sue445 <sue445@sue445.net>
2021-10-15 09:33:04 +0900

    :golf:

--- lib/rubocop/isucon/database_connection.rb
+++ lib/rubocop/isucon/database_connection.rb
@@ -16,27 +16,20 @@ module RuboCop
       # @param table_name [String]
       # @return [Array<String>]
       def column_names(table_name)
-        return @column_names_by_table[table_name] if @column_names_by_table[table_name]
-
-        columns = ActiveRecord::Base.connection.columns(table_name)
-        @column_names_by_table[table_name] = columns.map(&:name)
+        @column_names_by_table[table_name] ||= ActiveRecord::Base.connection.columns(table_name).map(&:name)
       end
 
       # @param table_name [String]
       # @return [Array<ActiveRecord::ConnectionAdapters::IndexDefinition>]
       # @see https://github.com/rails/rails/blob/v6.1.4.1/activerecord/lib/active_record/connection_adapters/abstract/schema_definitions.rb#L8
       def indexes(table_name)
-        return @indexes_by_table[table_name] if @indexes_by_table[table_name]
-
-        @indexes_by_table[table_name] = ActiveRecord::Base.connection.indexes(table_name)
+        @indexes_by_table[table_name] ||= ActiveRecord::Base.connection.indexes(table_name)
       end
 
       # @param table_name [String]
       # @return [Array<String>] primary key's column names
       def primary_keys(table_name)
-        return @primary_keys_by_table[table_name] if @primary_keys_by_table[table_name]
-
-        @primary_keys_by_table[table_name] = ActiveRecord::Base.connection.primary_keys(table_name)
+        @primary_keys_by_table[table_name] ||= ActiveRecord::Base.connection.primary_keys(table_name)
       end
     end
   end
