2d2fa201a624c0e80c7e8d3db54e22578897bd2f
sue445 <sue445@sue445.net>
2021-12-07 00:08:41 +0900

    Fixed. Metrics/MethodLength

--- lib/rubocop/cop/isucon/mysql2/select_asterisk.rb
+++ lib/rubocop/cop/isucon/mysql2/select_asterisk.rb
@@ -42,15 +42,26 @@ module RuboCop
               next unless gda.ast.respond_to?(:expr_list)
 
               gda.ast.expr_list.each do |select_field_node|
-                next if !select_field_node.respond_to?(:expr) || select_field_node.expr.value != "*"
-
-                loc = offense_location(type: type, node: node, gda_location: select_field_node.expr.location)
-                next unless loc
-
-                add_offense(loc) do |corrector|
-                  perform_autocorrect(corrector: corrector, loc: loc, gda: gda)
-                end
+                check_and_register_offence_for_select_field_node(
+                  type: type, node: node, gda: gda,
+                  select_field_node: select_field_node
+                )
               end
+            end
+          end
+
+          # @param type [Symbol] Node type. one of `:str`, `:dstr`
+          # @param node [RuboCop::AST::Node]
+          # @param gda [RuboCop::Isucon::GDA::Client]
+          # @param select_field_node [GDA::Nodes::SelectField]
+          def check_and_register_offence_for_select_field_node(type:, node:, gda:, select_field_node:)
+            return if !select_field_node.respond_to?(:expr) || select_field_node.expr.value != "*"
+
+            loc = offense_location(type: type, node: node, gda_location: select_field_node.expr.location)
+            return unless loc
+
+            add_offense(loc) do |corrector|
+              perform_autocorrect(corrector: corrector, loc: loc, gda: gda)
             end
           end
 
