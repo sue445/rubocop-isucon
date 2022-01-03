e2950e60ee119246533ac44244c392b0e5842d04
sue445 <sue445@sue445.net>
2022-01-04 00:48:36 +0900

    Change offense position

--- spec/rubocop/cop/isucon/sinatra/serve_static_file_spec.rb
+++ spec/rubocop/cop/isucon/sinatra/serve_static_file_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe RuboCop::Cop::Isucon::Sinatra::ServeStaticFile, :config do
       # c.f. https://github.com/isucon/isucon8-final/blob/38c4f6e20388d1c4f1ed393fb75b38d472e44abf/webapp/ruby/app.rb#L55-L58
       expect_offense(<<~RUBY)
         get '/' do
+        ^^^^^^^^^^ Serve static files on front server (e.g. nginx)
           content_type :html
           File.read(File.join(__dir__, '..', 'public', 'index.html'))
-          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Serve static files on front server (e.g. nginx)
         end
       RUBY
     end
