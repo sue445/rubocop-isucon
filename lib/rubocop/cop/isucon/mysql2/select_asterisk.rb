33374b70c53fa3fba7b194d50c2ad3cde58fb6dc
sue445 <sue445@sue445.net>
2022-04-05 21:56:52 +0900

    Write note

--- lib/rubocop/cop/isucon/mysql2/select_asterisk.rb
+++ lib/rubocop/cop/isucon/mysql2/select_asterisk.rb
@@ -8,6 +8,10 @@ module RuboCop
         #
         # @note If `Database` isn't configured, auto-correct will not be available. (Only offense detection can be used)
         #
+        # @note This cop replaces `SELECT *` with a `SELECT` by the columns present in the table (e.g. `SELECT id, name`),
+        #       but does not check whether the columns are actually used.
+        #       Please manually delete unused columns after auto corrected
+        #
         # @example
         #   # bad
         #   db.xquery('SELECT * FROM users')
