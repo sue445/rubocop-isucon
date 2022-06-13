71e4290b49a344c01ead32550e92ec66280f21b9
sue445 <sue445@sue445.net>
2022-06-13 10:55:08 +0900

    Update yard

--- lib/rubocop/cop/isucon/mysql2/prepare_execute.rb
+++ lib/rubocop/cop/isucon/mysql2/prepare_execute.rb
@@ -4,44 +4,18 @@ module RuboCop
   module Cop
     module Isucon
       module Mysql2
-        # TODO: Write cop description and example of bad / good code. For every
-        # `SupportedStyle` and unique configuration, there needs to be examples.
-        # Examples must have valid Ruby syntax. Do not use upticks.
+        # Use `db.xquery` instead of `db.prepare`
         #
-        # @safety
-        #   Delete this section if the cop is not unsafe (`Safe: false` or
-        #   `SafeAutoCorrect: false`), or use it to explain how the cop is
-        #   unsafe.
-        #
-        # @example EnforcedStyle: bar (default)
-        #   # Description of the `bar` style.
-        #
+        # @example
         #   # bad
-        #   bad_bar_method
-        #
-        #   # bad
-        #   bad_bar_method(args)
+        #   db.prepare('SELECT * FROM `users` WHERE `id` = ?').execute(
+        #     session[:user][:id]
+        #   ).first
         #
         #   # good
-        #   good_bar_method
-        #
-        #   # good
-        #   good_bar_method(args)
-        #
-        # @example EnforcedStyle: foo
-        #   # Description of the `foo` style.
-        #
-        #   # bad
-        #   bad_foo_method
-        #
-        #   # bad
-        #   bad_foo_method(args)
-        #
-        #   # good
-        #   good_foo_method
-        #
-        #   # good
-        #   good_foo_method(args)
+        #   db.xquery('SELECT * FROM `users` WHERE `id` = ?',
+        #     session[:user][:id]
+        #   ).first
         #
         class PrepareExecute < Base
           extend AutoCorrector
