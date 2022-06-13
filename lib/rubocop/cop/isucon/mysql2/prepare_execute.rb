a8ff555eb443fa2f73e1eb423751f0d88098e0d4
sue445 <sue445@sue445.net>
2022-06-13 10:57:32 +0900

    [Resolved] Performance/FixedSize

--- lib/rubocop/cop/isucon/mysql2/prepare_execute.rb
+++ lib/rubocop/cop/isucon/mysql2/prepare_execute.rb
@@ -22,6 +22,8 @@ module RuboCop
 
           MSG = "Use `db.xquery` instead of `db.prepare`"
 
+          EXECUTE_LENGTH = "execute(".length
+
           # @!method find_prepare_execute(node)
           def_node_search :find_prepare_execute, <<~PATTERN
             (send
@@ -73,7 +75,7 @@ module RuboCop
           def perform_autocorrect(corrector:, current_node:, prepare_arg_node:)
             prepare_begin_pos = current_node.child_nodes[0].loc.selector.begin_pos
             execute_begin_pos = current_node.child_nodes[0].loc.end.end_pos + 1
-            execute_end_pos = execute_begin_pos + "execute(".length
+            execute_end_pos = execute_begin_pos + EXECUTE_LENGTH
             loc = Parser::Source::Range.new(current_node.loc.expression.source_buffer, prepare_begin_pos, execute_end_pos)
 
             corrector.replace(loc, "xquery(#{prepare_arg_node.source},")
