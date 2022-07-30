1ebdf431e2b69e38c5f57186b08e71b7c83d972c
sue445 <sue445@sue445.net>
2022-07-30 17:07:28 +0900

    Refactor: Extract to OffenceLocationMethods

--- lib/rubocop/cop/isucon_cops.rb
+++ lib/rubocop/cop/isucon_cops.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "isucon/mixin/database_methods"
+require_relative "isucon/mixin/offense_location_methods"
 require_relative "isucon/mixin/mysql2_xquery_methods"
 require_relative "isucon/mixin/select_asterisk_methods"
 require_relative "isucon/mixin/sinatra_methods"
