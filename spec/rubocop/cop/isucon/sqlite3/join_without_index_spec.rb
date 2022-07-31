e5c07229ae339dfbbc486703cf078d58e36c747d
sue445 <sue445@sue445.net>
2022-07-31 13:03:21 +0900

    Impl JoinWithoutIndex

--- spec/rubocop/cop/isucon/sqlite3/join_without_index_spec.rb
+++ spec/rubocop/cop/isucon/sqlite3/join_without_index_spec.rb
@@ -1,21 +1,54 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Isucon::Sqlite3::JoinWithoutIndex, :config do
-  let(:config) { RuboCop::Config.new }
+  let(:config) { RuboCop::Config.new("Isucon/Sqlite3/JoinWithoutIndex" => cop_config) }
+  let(:cop_config) { {} }
 
-  # TODO: Write test code
-  #
-  # For example
-  it "registers an offense when using `#bad_method`" do
-    expect_offense(<<~RUBY)
-      bad_method
-      ^^^^^^^^^^ Use `#good_method` instead of `#bad_method`.
-    RUBY
+  context "without index" do
+    include_context :database_cop do
+      let(:schema) do
+        %w[
+          schemas/create_courses.rb
+          schemas/create_registrations.rb
+        ]
+      end
+    end
+
+    it "registers an offense" do
+      expect_offense(<<~RUBY)
+        db.execute(
+          "SELECT `courses`.*" \\
+          " FROM `courses`" \\
+          " JOIN `registrations` ON `courses`.`id` = `registrations`.`course_id`" \\
+                                                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^ This join clause doesn't seem to have an index. (e.g. `CREATE INDEX index_registrations_course_id ON registrations (course_id)`)
+          " WHERE `courses`.`status` != ? AND `registrations`.`user_id` = ?",
+          STATUS_CLOSED, user_id,
+        )
+      RUBY
+    end
   end
 
-  it "does not register an offense when using `#good_method`" do
-    expect_no_offenses(<<~RUBY)
-      good_method
-    RUBY
+  context "with index" do
+    include_context :database_cop do
+      let(:schema) do
+        %w[
+          schemas/create_courses.rb
+          schemas/create_registrations.rb
+          schemas/add_index_to_registrations.rb
+        ]
+      end
+    end
+
+    it "does not register an offense" do
+      expect_no_offenses(<<~RUBY)
+        db.execute(
+          "SELECT `courses`.*" \\
+          " FROM `courses`" \\
+          " JOIN `registrations` ON `courses`.`id` = `registrations`.`course_id`" \\
+          " WHERE `courses`.`status` != ? AND `registrations`.`user_id` = ?",
+          STATUS_CLOSED, user_id,
+        )
+      RUBY
+    end
   end
 end
