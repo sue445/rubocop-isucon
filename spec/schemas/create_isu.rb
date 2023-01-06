e18c1d8081a5024eb1bfcab14db157a0ed7962e8
sue445 <sue445@sue445.net>
2023-01-06 19:23:33 +0900

    :cop: rubocop --autocorrect-all --only Style/YodaExpression

--- spec/schemas/create_isu.rb
+++ spec/schemas/create_isu.rb
@@ -6,7 +6,7 @@ ActiveRecord::Schema.define(version: 1) do
   create_table :isu do |t|
     t.string :jia_isu_uuid
     t.string :name
-    t.column :image, :binary, limit: 16 * (1024**2) # 16MB
+    t.column :image, :binary, limit: (1024**2) * 16 # 16MB
     t.string :character
     t.string :jia_user_id
 
