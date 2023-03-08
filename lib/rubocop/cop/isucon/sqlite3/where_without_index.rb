48aaef01d677eb0a8cd5e4b4872bbcd79a8e2292
sue445 <sue445@sue445.net>
2023-03-08 21:01:45 +0900

    Fix typo in doc

--- lib/rubocop/cop/isucon/sqlite3/where_without_index.rb
+++ lib/rubocop/cop/isucon/sqlite3/where_without_index.rb
@@ -10,10 +10,10 @@ module RuboCop
         #
         # @example
         #   # bad (user_id is not indexed)
-        #   db.execute('SELECT id, title FROM articles WHERE used_id = ?', [user_id])
+        #   db.execute('SELECT id, title FROM articles WHERE user_id = ?', [user_id])
         #
         #   # good (user_id is indexed)
-        #   db.execute('SELECT id, title FROM articles WHERE used_id = ?', [user_id])
+        #   db.execute('SELECT id, title FROM articles WHERE user_id = ?', [user_id])
         #
         #   # good (id is primary key)
         #   db.execute('SELECT id, title FROM articles WHERE id = ?', [id])
