0ae2807a3fa384e9e5e4e9e0568feb7020c3b091
sue445 <sue445@sue445.net>
2025-03-23 22:13:13 +0900

    Use `RuboCop::Isucon::Plugin` instead of `RuboCop::Isucon::Inject`

--- lib/rubocop-isucon.rb
+++ lib/rubocop-isucon.rb
@@ -6,11 +6,9 @@ require "gda"
 
 require_relative "rubocop/isucon"
 require_relative "rubocop/isucon/version"
-require_relative "rubocop/isucon/inject"
+require_relative "rubocop/isucon/plugin"
 require_relative "rubocop/isucon/database_connection"
 require_relative "rubocop/isucon/memorize_methods"
 require_relative "rubocop/isucon/gda"
-
-RuboCop::Isucon::Inject.defaults!
 
 require_relative "rubocop/cop/isucon_cops"
